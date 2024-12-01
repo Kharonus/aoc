5e11683b382dfda249a1e65658e4d053bff4299a
Eric Schubert <e.schubert@posteo.de>
2024-12-02 00:22:09 +0100

    added aoc 2024-1

---
+++ rust/aoc/src/year2024/day1.rs
@@ -0,0 +1,43 @@
+pub fn solve_first_star(input: &str) -> String {
+    let (mut left, mut right) = parse_input(input);
+
+    left.sort();
+    right.sort();
+
+    let distances = left
+        .iter()
+        .zip(right.iter())
+        .fold(0, |acc, (l, r)| acc + (l - r).abs());
+
+    format!("{}", distances)
+}
+
+pub fn solve_second_star(input: &str) -> String {
+    let (left, right) = parse_input(input);
+
+    let similarity = left.iter().fold(0, |acc, &number| {
+        let x = right.iter().filter(|&&r| r == number).count() as i32;
+        acc + (number * x)
+    });
+
+    format!("{}", similarity)
+}
+
+fn parse_input(input: &str) -> (Vec<i32>, Vec<i32>) {
+    input.lines().fold(
+        (Vec::new(), Vec::new()),
+        |mut acc: (Vec<i32>, Vec<i32>), line| {
+            let mut s = line.split_whitespace();
+
+            if let Some(left) = s.next() {
+                acc.0.push(left.parse().unwrap());
+            }
+
+            if let Some(right) = s.next() {
+                acc.1.push(right.parse().unwrap());
+            }
+
+            acc
+        },
+    )
+}
